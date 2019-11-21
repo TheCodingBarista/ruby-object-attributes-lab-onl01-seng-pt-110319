357cceefe25b37448bdd9b53c2c2a5e040f5fdc2
Melissa Holt <melissa.holt21@gmail.com>
2019-11-21 17:51:11 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,25 @@
+class Person
+  
+  def intitialize(name, breed)
+    @name = name
+    @breed = breed
+  end
+  
+  def name=(name) 
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+
+  
+  def breed=(breed)
+    @breed = breed
+  end
+  
+  def breed
+    @breed
+  end
+  
+end
